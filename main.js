8e6ba9a0bdc3bc1de785a91a3693d25c6fa3d56b
spsivavin <103201497+spsivavin@users.noreply.github.com>
2022-11-03 20:01:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function praloaded()  {
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
